1914079815901c047ae2944d4d773d83d708d8cf
KristianMB13 <kristian.bergedalen@hotmail.com>
2024-11-18 16:08:16 +0100

    Upload new project files for FullDataBase

--- WebApplication1/Models/UserData.cs
+++ WebApplication1/Models/UserData.cs
@@ -1,10 +1,16 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class UserData
     {
+        public int Id { get; set; }
         public string? Name {  get; set; }
         public string? Email { get; set; }
-        public string? Phone {  get; set; }
+        public string? PhoneNumber {  get; set; }
         public string? Address { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
     }
 }
