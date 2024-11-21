b5fef14a4d63e41189353536c06c6cbb85048a67
KristianMB13 <kristian.bergedalen@hotmail.com>
2024-11-21 11:53:03 +0100

    si

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
 
 namespace WebApplication1.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -11,12 +13,21 @@ namespace WebApplication1.Data
 
         // DbSet for GeoChange
         public DbSet<GeoChange> GeoChanges { get; set; }
-        
-        
+
         // DbSet for UserData
         public DbSet<UserData> UserData { get; set; }
 
-
+        // DbSet for AreaChange
         public DbSet<AreaChange> AreaChange { get; set; }
+
+        // Additional configuration can be added here if necessary
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Example of customizing Identity tables or relationships
+            // builder.Entity<IdentityUser>().ToTable("MyUsers"); // Renaming IdentityUser table
+        }
     }
 }
+
